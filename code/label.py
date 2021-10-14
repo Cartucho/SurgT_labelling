ed74273ef76296df5eab6734dad08ddd904d5c07
Cartucho <joaocartuchoo@gmail.com>
2021-10-14 19:26:17 +0100

    Adding mouse tracking to the interface.

--- code/label.py
+++ code/label.py
@@ -30,6 +30,8 @@ class Interface:
         # Initialize
         self.ind_im = 0
         self.ind_class = 0
+        self.mouse_u = 0
+        self.mouse_v = 0
 
     def load_image_paths(self):
         im_l_path = os.path.join(self.dir_l, "*{}".format(self.im_format))
@@ -38,7 +40,7 @@ class Interface:
         self.im_path_r = natsorted(glob.glob(im_r_path))
 
 
-    def resize_images(self, im_l, im_r):
+    def im_resize(self, im_l, im_r):
         im_w_c = im_l.shape[1]
         im_w_d = self.im_w_d
         f = float(im_w_d) / im_w_c # scale factor
@@ -47,25 +49,44 @@ class Interface:
         return im_l, im_r
 
 
-    def show_image(self):
+    def im_augmentation(self, im_l, im_r):
+        return im_l, im_r
+
+
+    def mouse_listener(self, event, x, y, flags, param):
+        self.mouse_u = y
+        self.mouse_v = x
+
+
+    def create_window(self, window_name):
+        cv.namedWindow(window_name, cv.WINDOW_KEEPRATIO)
+        cv.setMouseCallback(window_name, self.mouse_listener)
+
+
+    def im_get(self):
         im_path_l = self.im_path_l[self.ind_im] # TODO: update ind_im carefully
         im_path_r = self.im_path_r[self.ind_im]
         im_l = cv.imread(im_path_l, -1)
         im_r = cv.imread(im_path_r, -1)
         # Check that images have the same size
         assert(im_l.shape == im_r.shape)
+        # Create image window
+        #cv2.resizeWindow(WINDOW_NAME, 1000, 700)
         # Resize images (from current to desired)
-        im_l, im_r = self.resize_images(im_l, im_r)
-        # Stack images together
-        stack = np.concatenate((im_l, im_r), axis=1)
-        cv.imshow("Stereo match labeler", stack)
+        im_l, im_r = self.im_resize(im_l, im_r)
+        # Augment images
+        #im_l, im_r = self.im_augmentation(im_l, im_r)
+        return im_l, im_r
 
 
-    def main_loop(self):
+    def main_loop(self, window_name):
         """ Interface's main loop """
         key_pressed = None
         while key_pressed != ord(self.key_quit):
-            self.show_image()
+            im_l, im_r = self.im_get()
+            # Stack images together
+            stack = np.concatenate((im_l, im_r), axis=1)
+            cv.imshow(window_name, stack)
             key_pressed = cv.waitKey(20)
         print("Finished")
 
@@ -73,4 +94,6 @@ class Interface:
 def label_data(config):
     inter = Interface(config)
     inter.load_image_paths()
-    inter.main_loop()
+    window_name = "Stereo match labeler"
+    inter.create_window(window_name)
+    inter.main_loop(window_name)
