c07b74f0cf335dc65be323a1cebc984f024b2182
Cartucho <joaocartuchoo@gmail.com>
2021-10-14 17:29:56 +0100

    Starting interface class

--- code/label.py
+++ code/label.py
@@ -1,2 +1,13 @@
+class Interface:
+    def __init__(self, config):
+        print(config)
+
+    def main_loop(self):
+        """ Interface's main loop """
+        print("Finished")
+
+
 def label_data(config):
     print(config)
+    inter = Interface(config)
+    inter.main_loop()
